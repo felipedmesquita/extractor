c65d5d33bd04d920dbe4fea4f5b9aaaa3e5e389a
felipedmesquita <16197684+felipedmesquita@users.noreply.github.com>
2024-01-28 10:38:42 -0300

    array parameter test

--- test/tap_test.rb
+++ test/tap_test.rb
@@ -12,4 +12,16 @@ class TapTest < Minitest::Test
     tap = Extractor::Tap.new
     assert_equal 148, tap.next_value(current_value: 147, parameter: nil, request_for_batch_size: nil)
   end
+
+  # when parameter is an array
+  def test_next_value_when_parameter_is_an_array
+    tap = Extractor::Tap.new
+
+    items_to_be_extracted = [1, 2, 3, 4]
+    batch_size = 2
+    current_batch = [1, 2]
+    expected_next_batch = [3, 4]
+
+    assert_equal expected_next_batch, tap.next_value(current_value: current_batch, parameter: items_to_be_extracted, request_for_batch_size: batch_size)
+  end
 end
